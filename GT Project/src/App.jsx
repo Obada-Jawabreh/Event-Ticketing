d0ff381d4f008460fbf5c605b8ed4231c6f771bf
hashem-frehat <hashem.abofreh@gmail.com>
2024-07-18 22:27:06 +0300

    first nav

--- GT Project/src/App.jsx
+++ GT Project/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
-
+import Navbar from "./Components/Layout/Navbar";
 function App() {
   return (
     <>
-      <h1>hello GT </h1>
+      <Navbar />
     </>
   );
 }
