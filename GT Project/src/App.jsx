0fc6f3003fb711b5f65d1cffa0b1b6611d2a3605
hashem-frehat <hashem.abofreh@gmail.com>
2024-07-19 17:01:40 +0300

    checkout first update

--- GT Project/src/App.jsx
+++ GT Project/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Checkout from "./Pages/Check out";
 import Navbar from "./Components/Layout/Navbar";
 function App() {
   return (
     <>
       <Navbar />
+      <Checkout />
     </>
   );
 }
