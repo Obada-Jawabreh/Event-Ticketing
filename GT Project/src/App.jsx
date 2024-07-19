3249d23bfd25e828897198b67cc4cf40f6262dbf
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-19 17:09:12 +0300

    add routing to app

--- GT Project/src/App.jsx
+++ GT Project/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import Catalog from "./Pages/Catalog";
+import LogIn from "./Pages/Log in";
+import SiginUp from "./Pages/Sigin up";
 function App() {
   return (
-    <>
-      <h1>hello GT </h1>
-    </>
+    <div className="bg-prim-dark">
+      <BrowserRouter>
+        <Routes>
+          <Route path="signup" element={<SiginUp />} />
+          <Route path="login" element={<LogIn />} />
+          <Route path="Catalog" element={<Catalog />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
