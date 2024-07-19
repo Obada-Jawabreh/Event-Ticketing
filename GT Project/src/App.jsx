8abd37fc5cb987540828f21a3851e9cf4b9a8f08
Obada-Jawabreh <obadajawabreh6@gmail.com>
2024-07-19 17:05:34 +0300

    add CRUD

--- GT Project/src/App.jsx
+++ GT Project/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import FetchEvents from "./Hooks/getEvents.jsx";
+import { dbURL } from "./FirebaseConfig/Config";
+import CreateEvent from "./Hooks/createEvent.jsx";
+import EditEvent from "./Hooks/updateEvent.jsx";
+import { useEffect } from "react";
+import DeleteEvent from "./Hooks/deleteEvent.jsx";
+import {SignUpComponent, LoginComponent} from "./log.jsx";
+
 
 function App() {
+
+
   return (
     <>
-      <h1>hello GT </h1>
     </>
   );
 }
