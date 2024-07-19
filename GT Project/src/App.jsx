f18cca0692345b4b91f3632504457222411f6315
3la2i <alaa.ata25@gmail.com>
2024-07-19 17:05:43 +0300

    detaillspage

--- GT Project/src/App.jsx
+++ GT Project/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import EventDetails from "./Pages/Details";
 
 function App() {
   return (
     <>
-      <h1>hello GT </h1>
+      {/* <h1>hello GT </h1> */}
+      <EventDetails/>
     </>
   );
 }
