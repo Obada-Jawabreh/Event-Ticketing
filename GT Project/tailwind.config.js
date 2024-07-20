e6a747af37ec7ca92de48b772df35753ae82f4ab
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-07-20 17:25:00 +0300

    Enable searching bar

--- GT Project/tailwind.config.js
+++ GT Project/tailwind.config.js
@@ -10,11 +10,16 @@ const config = {
         pin: "#FF204E",
         "prim-dark": "#221B2C",
         "second-dark": "#31273F",
+        "ter-dark": "#423760",
         "custom-red": "#FF204E",
-        "custom-red-hover": "#FF0035",
+        "custom-red-hover": "#d4054c",
       },
       width: {
         "fill-available": "-webkit-fill-available",
+      },
+      backgroundImage: {
+        "gradient-four-colors":
+          "linear-gradient(220deg, rgba(49,39,63,1) -1%, rgba(78,67,118,0.6083683473389356) 93%)",
       },
     },
   },
